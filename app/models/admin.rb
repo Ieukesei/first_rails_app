02d938bccfc1a038eba2f7e6e46e2bd046437457
Ieukesei <fercrey@gmail.com>
2015-03-17 19:12:02 +0100

    17_03_2015

--- app/models/admin.rb
+++ app/models/admin.rb
@@ -1,6 +1,15 @@
 class Admin < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :registerable,
+  devise :database_authenticatable,
          :recoverable, :rememberable, :trackable, :validatable
+
+
+  validates :username,
+  :presence => true,
+  :uniqueness => {
+    :case_sensitive => false
+  } 
+
+
 end
